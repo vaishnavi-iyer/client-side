b52a4bde1d378b9ac855f67b672c79e92a75c294
Vaishnavi Iyer <email": null>
2016-06-28 11:09:11 +1200

    added working api and multiple calls

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ xhr.get(endpoint, function (err, data) {
     console.error(err)
   }
 
-  // In case you're curious
-  console.log(data.body) // FYI: data.body is a string
-
+  var data = JSON.parse(data.body)
+  var newendpoint = endpoint+ '/'+ data[0].id
+  xhr.get(newendpoint, function(err, data){
+    if (err){
+      console.log(err)
+    }
   // Replace 'Space' below with the response
-  var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting({name: 'Space'})
+    var newdata = JSON.parse(data.body)
+    for (var key in newdata){
+      var target = document.getElementsByTagName('main')[0]
+      target.innerHTML += greeting({name: key + ' : ' + newdata[key]})
+    }
+  })
 })
